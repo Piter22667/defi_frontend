20e4aece9ffe66acdcc508d2a2be942130a26b41
Piter Bilyk <bilykpiter492@gmail.com>
2025-06-01 20:40:01 +0300

    fixed menu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,112 +4,131 @@ import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
 import QueryStatsOutlinedIcon from '@mui/icons-material/QueryStatsOutlined';
 import BubbleChartOutlinedIcon from '@mui/icons-material/BubbleChartOutlined';
 import UserBlock from "./UserBlock";
-import {Link} from "react-router-dom";
+import {Link, useLocation, NavLink} from "react-router-dom";
 
 
-const Sidebar = ({ user }) => (
 
-    <Box
-        sx={{
-            width: { xs: 64, sm: 180, md: 220 },
-            minWidth: { xs: 64, sm: 180, md: 220 },
-            bgcolor: "#20222B",
-            boxShadow: "2px 0 16px 0 rgba(0,0,0,0.25)",
-            py: 3,
-            px: { xs: 0, sm: 2 },
-            height: { xs: "20%", sm: "30%", md: "80%" },
-            minHeight: "100vh",
-            display: "flex",
-            flexDirection: "column",
-            borderRadius: 3,
-            mr: { xs: 1, sm: 2, md: 4 },
-            transition: "width 0.2s",
-        }}
-    >
-        <Typography
-            variant="h6"
+const Sidebar = ({ user }) => {
+
+    const location = useLocation();
+
+    return (
+        <Box
             sx={{
-                color: "#fff",
-                mb: 4,
-                fontWeight: 700,
-                textAlign: "center",
-                fontSize: { xs: 0, sm: 18 },
-                height: { xs: 0, sm: "auto" },
-                overflow: "hidden",
-                transition: "font-size 0.2s",
+                width: {xs: 64, sm: 180, md: 220},
+                minWidth: {xs: 64, sm: 180, md: 220},
+                bgcolor: "#20222B",
+                boxShadow: "2px 0 16px 0 rgba(0,0,0,0.25)",
+                py: 3,
+                px: {xs: 0, sm: 2},
+                height: {xs: "20%", sm: "30%", md: "80%"},
+                minHeight: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                borderRadius: 3,
+                mr: {xs: 1, sm: 2, md: 4},
+                transition: "width 0.2s",
             }}
         >
-            DeFi App
-        </Typography>
-        <List>
-            <ListItem button selected sx={{
-                bgcolor: "#23262F",
-                mb: 2,
-                color: "#fff",
-                fontWeight: 600,
-                fontSize: 18,
-                borderRadius: 2,
-                '&.Mui-selected, &:hover': {
-                    bgcolor: "#23262F",
-                    color: "#00FFF0",
-                },
-                px: { xs: 1, sm: 2 }
-            }}
-                      component={Link} to="/"
+            <Typography
+                variant="h6"
+                sx={{
+                    color: "#fff",
+                    mb: 4,
+                    fontWeight: 700,
+                    textAlign: "center",
+                    fontSize: {xs: 0, sm: 18},
+                    height: {xs: 0, sm: "auto"},
+                    overflow: "hidden",
+                    transition: "font-size 0.2s",
+                }}
             >
-                <ListItemIcon sx={{ color: "#00FFF0", minWidth: 0, justifyContent: "center", px:{ xs: 0, sm: 2 } }}>
-                    <DashboardOutlinedIcon fontSize="medium" />
-                </ListItemIcon>
-                <ListItemText
-                    primary="Dashboard"
+                DeFi App
+            </Typography>
+            <List>
+                <ListItem
+                  component={NavLink}
+                  to="/"
+                  end
+                  sx={{
+                      mb: 2,
+                      color: "#fff",
+                      fontWeight: 600,
+                      fontSize: 18,
+                      borderRadius: 2,
+                      '&.active, &:hover': {
+                          bgcolor: "#23262F",
+                          color: "#00FFF0",
+                      },
+                      px: {xs: 1, sm: 2}
+                  }}
+                >
+                    <ListItemIcon sx={{color: "#00FFF0", minWidth: 0, justifyContent: "center", px: {xs: 0, sm: 2}}}>
+                        <DashboardOutlinedIcon fontSize="medium"/>
+                    </ListItemIcon>
+                    <ListItemText
+                        primary="Dashboard"
+                        sx={{
+                            display: {xs: "none", sm: "block"}
+                        }}
+                    />
+                </ListItem>
+                <ListItem
+                    component={NavLink}
+                    to="/dex-charts"
                     sx={{
-                        display: { xs: "none", sm: "block" }
+                        mb: 2,
+                        color: "#fff",
+                        borderRadius: 2,
+                        fontWeight: 600,
+                        fontSize: 18,
+                        '&.active, &:hover': {
+                            bgcolor: "#23262F",
+                            color: "#00FFF0",
+                        },
+                        px: {xs: 1, sm: 2}
                     }}
-                />
-            </ListItem>
-            <ListItem button sx={{
-                mb: 2,
-                color: "#fff",
-                borderRadius: 2,
-                '&:hover': { bgcolor: "#23262F", color: "#00FFF0" },
-                px: { xs: 1, sm: 2 }
-            }}
-                      component={Link} to="/dex-charts"
-            >
-                <ListItemIcon sx={{ color: "#fff", minWidth: 0, justifyContent: "center" , px:{ xs: 0, sm: 2 } }} >
-                    <QueryStatsOutlinedIcon fontSize="medium" />
-                </ListItemIcon>
-                <ListItemText
-                    primary="Dex Protocol detailed charts"
-                    sx={{
-                        display: { xs: "none", sm: "block" }
-                    }}
-                />
-            </ListItem>
-            <ListItem button sx={{
-                color: "#fff",
-                borderRadius: 2,
-                '&:hover': { bgcolor: "#23262F", color: "#00FFF0" },
-                px: { xs: 1, sm: 2 }
-            }}
-                      component={Link} to="/getAddressTxs"
-            >
-                <ListItemIcon sx={{ color: "#fff", minWidth: 0, justifyContent: "center", px:{ xs: 0, sm: 2 } }}>
-                    <BubbleChartOutlinedIcon fontSize="medium" />
-                </ListItemIcon>
-                <ListItemText
-                    primary="Get Addresss txs"
-                    sx={{
-                        display: { xs: "none", sm: "block" }
-                    }}
-                />
-            </ListItem>
-        </List>
-        {/*<Box sx={{ flexGrow: 0.5 }} />*/}
-        <Box sx={{ px: { xs: 0, sm: 1 } }}>
-            <UserBlock user={user} />
-        </Box>
-    </Box>
-);
 
+                >
+                    <ListItemIcon sx={{color: "#fff", minWidth: 0, justifyContent: "center", px: {xs: 0, sm: 2}}}>
+                        <QueryStatsOutlinedIcon fontSize="medium"/>
+                    </ListItemIcon>
+                    <ListItemText
+                        primary="Dex Protocol detailed charts"
+                        sx={{
+                            display: {xs: "none", sm: "block"}
+                        }}
+                    />
+                </ListItem>
+                <ListItem
+                    component={NavLink}
+                    to="/getAddressTxs"
+
+                    sx={{
+                    color: "#fff",
+                    borderRadius: 2,
+                    '&.active ,&:hover': {
+                        bgcolor: "#23262F",
+                        color: "#00FFF0"},
+                    px: {xs: 1, sm: 2}
+                }}
+                >
+                    <ListItemIcon sx={{color: "#fff", minWidth: 0, justifyContent: "center", px: {xs: 0, sm: 2}}}>
+                        <BubbleChartOutlinedIcon fontSize="medium"/>
+                    </ListItemIcon>
+                    <ListItemText
+                        primary="Get Addresss txs"
+                        sx={{
+                            display: {xs: "none", sm: "block"}
+                        }}
+                    />
+                </ListItem>
+            </List>
+            {/*<Box sx={{ flexGrow: 0.5 }} />*/}
+            <Box sx={{px: {xs: 0, sm: 1}}}>
+                <UserBlock user={user}/>
+            </Box>
+        </Box>
+    );
+}
 export default Sidebar;
